9d2d40aa5e7f8b7ac4a460d568a0e800449c756a
Melle Moerkerk <melle@vxl.sh>
2026-08-06 16:52:22 +0200

    Fix no page rendered on login backend error

--- web/src/routes/login/+layout.ts
+++ web/src/routes/login/+layout.ts
@@ -1,7 +1,11 @@
 import type { LayoutLoad } from './$types';
+import { error } from '@sveltejs/kit';
 import client from '$lib/api';
 
 export const load: LayoutLoad = async ({ fetch }) => {
-	const { data } = await client.GET('/api/v1/auth/metadata', { fetch: fetch });
+	const { data, response } = await client.GET('/api/v1/auth/metadata', { fetch: fetch });
+	if (!response.ok) {
+		throw error(503);
+	}
 	return { loginData: data };
 };
